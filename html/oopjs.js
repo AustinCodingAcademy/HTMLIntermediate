6848d64ebadb61439cd662ad50e69a21f161871d
Rob Lowe <rhlowe@gmail.com>
2015-03-01 19:51:55 -0600

    Clearing this file

--- html/oopjs.js
+++
@@ -1,6 +0,0 @@
-function speak(line) {
-    console.log(`The ${this.type} cow says, "${line}".`);
-}
-
-var ongoleCow = { type: "Ongole", speak: speak };
-var herefordCow = { type: "Hereford", speak: speak };
