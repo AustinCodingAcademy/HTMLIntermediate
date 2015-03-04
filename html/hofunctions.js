fe57d78dd6b65c2f8f9e73775d5fd495864a8800
Rob <rlowe@w2ogroup.com>
2015-03-04 17:30:30 -0600

    in class exersices

--- html/hofunctions.js
+++ html/hofunctions.js
@@ -24,3 +24,55 @@ function unless(test, then) {
 function repeat(times, body) {
   for (var i = 0; i < times; i++) { body(i) };
 }
+
+function transparentWrapping(f) {
+    return function() {
+        return f.apply(null, arguments);
+    };
+}
+
+function rconsole(arg) {
+    arg.forEach( function(item){
+        console.log(item);
+    });
+}
+
+function filter(array, test) {
+    var passed = [];
+    for(var i = 0; i < array.length; i++) {
+        if (test(array[i])) {
+            passed.push(array[i]);
+        }
+    }
+    return passed;
+}
+
+var movies80s = movie_list.filter( function(movie) {
+    return movie.release_date > 1979 && movie.release_date < 1990;
+});
+
+function map(array, transform) {
+    var mapped = [];
+    for(var i = 0; i < array.length; i++) {
+        mapped.push(transform(array[i]));
+    }
+    return mapped;
+}
+
+directors80s = movies80s.map( function(movie) {
+    return movie.director;
+});
+
+function average(array) {
+    function plus(a, b) { return a + b; }
+    return array.reduce(plus) / array.length;
+}
+function year(movie) {
+    return movie.release_date;
+}
+function director(movie) {
+    return movie.director === "James Cameron";
+}
+function genre(movie) {
+    return movie.genre === "Sci-Fi";
+}
