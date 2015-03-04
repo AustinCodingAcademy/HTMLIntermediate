0f4279e6a3fd564d74545fe9baf227c8e9702d25
Rob Lowe <rhlowe@gmail.com>
2015-03-03 22:52:15 -0600

    starting the in-class exercise.

--- html/movies.js
+++ html/movies.js
@@ -1,3 +1,4 @@
+ "use strict";
 var movie_list = [
     {"title": "The Abyss", "genre": "Sci-Fi", "release_date": 1989, "director": "James Cameron"},
     {"title": "Alien", "genre": "Sci-Fi", "release_date": 1979, "director": "Ridley Scott"},
@@ -5,4 +6,4 @@ var movie_list = [
     {"title": "Field of Dreams", "genre": "Drama", "release_date": 1989, "director": "Phil Alden Robinson"},
     {"title": "The Incredibles", "genre": "Action", "release_date": 2004, "director": "Brad Bird"},
     {"title": "Sphere", "genre": "Sci-Fi", "release_date": 1998, "director": "Barry Levinson"}
-]
+];
