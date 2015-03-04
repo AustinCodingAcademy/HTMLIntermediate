cf39b3ecfedc00ad39b025766336cd728b9bc99f
Rob Lowe <rhlowe@gmail.com>
2015-03-03 18:54:05 -0600

    Add linter configuration

--- html/fizzbuzz.js
+++ html/fizzbuzz.js
@@ -1,10 +1,11 @@
+ "use strict";
+
 /**
  * Loop 1 to 100
  * if loop % 3 print fizz
  * if loop % 5 print buzz
  * if loop % 3 and 5 print fizzbuzz
  */
-
 function fizzbuzz() {
     console.time("fizzbuzz");
     for (var i = 1; i <= 100; i++) {
@@ -25,10 +26,10 @@ function fb() {
     console.time("fb");
     for (var i = 1; i <= 100; i++) {
         var fbstring = "";
-        if (0 === i % 3) {
+        if (i % 3 === 0) {
             fbstring += "fizz";
         }
-        if (0 === i % 5) {
+        if (i % 5 === 0) {
             fbstring += "buzz";
         }
         console.log(fbstring, i);
