132ba8ba11110ad63ae5ff483e4ce22c34999119
Rob <rhlowe@gmail.com>
2015-08-02 17:08:10 -0500

    Add files for working with Node.js, NPM, Gulp, and Babel

---
+++ support-files/gulp-test/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require("gulp");
+var sourcemaps = require("gulp-sourcemaps");
+var babel = require("gulp-babel");
+
+gulp.task('babel', function () {
+    return gulp.src("src/app.js")
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/*.js', ['babel'])
+});
+
+gulp.task('default', ['babel', 'watch'], function() {});
