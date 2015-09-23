3b75ccdd69164b54350e69841a6d93e2ca332e75
Rob <rhlowe@gmail.com>
2015-09-22 20:13:57 -0500

    Codify Gulp

--- support-files/gulp/gulpfile.js
+++ support-files/gulp/gulpfile.js
@@ -1,17 +1,26 @@
-var gulp = require("gulp");
-var sourcemaps = require("gulp-sourcemaps");
-var babel = require("gulp-babel");
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var eslint = require('gulp-eslint');
 
-gulp.task('babel', function () {
-    return gulp.src("src/*.js")
+gulp.task('babel', ['lint'], function () {
+    return gulp.src('src/**/*.js')
         .pipe(sourcemaps.init())
         .pipe(babel())
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("dist"));
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'))
+    ;
+});
+
+gulp.task('lint', function () {
+    return gulp.src(['src/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/*.js', ['babel']);
+    gulp.watch('src/*.js', ['lint', 'babel']);
 });
 
-gulp.task('default', ['babel', 'watch'], function() {});
+gulp.task('default', ['lint', 'babel', 'watch'], function() {});
